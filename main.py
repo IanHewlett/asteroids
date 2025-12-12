e348005369a7cf7e0144f226eca0c1d6e84adecc
IanHewlett <ian.w.hewlett@gmail.com>
2025-12-12 01:35:33 -0800

    shot collision impl

--- main.py
+++ main.py
@@ -42,8 +42,13 @@ def main():
 
         updatable.update(dt)
 
-        for item in asteroids:
-            if item.collides_with(player):
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+            if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
